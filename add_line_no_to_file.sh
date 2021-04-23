ac5479242843db85e3de46ea5981901ec1a9799c
Aftab Hussain <ahussa35@shiraz.hpcc.uh.edu>
2021-04-23 05:38:17 -0500

    lineno scrpt: get special chars

--- add_line_no_to_file.sh
+++ add_line_no_to_file.sh
@@ -3,11 +3,16 @@
 # $1 is the name of var-misuse dataset file
 
 line_no=0
+export IFS=
 
-while read p; do
+while read -r p; do
   let "line_no += 1"
-  sed "s/^{/{\"line_no\": $line_no, /g" <<< $p >> ${1}_linenum 
+  sed "s/^{/{\"line_no\": $line_no, /g" <<< "$p" >> ${1}_lineno 
 done < $1
 
 # Above, we use <<< to redirect a string literal to sed. Also, we use double
-# quotes for the sed string as we want to pass a bash variable.
+# quotes for the sed string instead of single quotes as we want to pass a bash
+# variable.
+
+# Also, we use -r, IFS= , and "" around $p in order to capture special chars. Ref:
+# https://stackoverflow.com/questions/11564778/bash-special-characters-lost-when-reading-file/11564954
