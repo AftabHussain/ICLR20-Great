3e4f707d45cf79700a39863d237fa48f15f3b31f
Aftab Hussain <ahussa35@shiraz.hpcc.uh.edu>
2021-08-10 15:23:55 -0500

    generate filename + lineno as sample id

--- add_line_no_to_file.sh
+++ add_line_no_to_file.sh
@@ -3,12 +3,20 @@
 # $1 is the name of var-misuse dataset file
 
 line_no=0
+
+# Remove forward slash from filename (so we can use it without issues with sed)
+# Ref: https://unix.stackexchange.com/questions/589312/shell-replace-forward-slashes-with-backslashes
+filename=$1
+filename_processed="${filename/\//-}"
+
 export IFS=
 
 while read -r p; do
   let "line_no += 1"
-  sed "s/^{/{\"line_no\": $line_no, /g" <<< "$p" >> ${1}_lineno 
+  sed "s/^{/{\"line_no\": \"${filename_processed}-${line_no}\", /g" <<< "$p" >> ${1}_lineno 
+  #sed "s/^{/{\"line_no\": ${line_no}, /g" <<< "$p" >> ${1}_lineno 
 done < $1
+
 
 # Above, we use <<< to redirect a string literal to sed. Also, we use double
 # quotes for the sed string instead of single quotes as we want to pass a bash
